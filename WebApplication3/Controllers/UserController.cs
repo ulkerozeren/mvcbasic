ec8599e2077ad0629c3679d2f5c22d90fd5aceef
ulkerozeren <53277896+ulkerozeren@users.noreply.github.com>
2019-09-07 15:00:35 +0300

    initilize

---
+++ WebApplication3/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Models.User>  userList;
+
+            using (DataContext dataContext = new DataContext())
+            {
+                userList = dataContext.Users.Select(x=>x).ToList();
+            }
+
+            return View(userList);
+        
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            using (DataContext dataContext = new DataContext())
+            {
+                dataContext.Users.Add(user);
+                dataContext.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication3/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            //DB BAĞLANTISI VB DATABASE INSTANCE'INI İLGİLENDİREN İNCE AYARLAR
+
+            // dbContextOptionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ECommerce;Integrated Security=True");
+            dbContextOptionsBuilder.UseSqlServer("Server=127.0.0.1;Database=Deneme;User=sa;Password=123;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+      
+    }
+}
